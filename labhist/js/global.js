03d64852fa4e5635b8ccaeb7ed7f4c4466bf67c4
Ryan Poe <baublet@gmail.com>
2018-06-30 15:33:14 -0400

    Add print stylesheets; better sidebar styling; and a way to show a full project exploration component to desktop views; and fix top content explorer component

--- labhist/js/global.js
+++ labhist/js/global.js
@@ -18,11 +18,30 @@ hammerSidebar.on("tap", function(ev) {
 	sidebar.classList.add("shown")
 })
 
-// Moving projects from the nav to across the top on desktop
+
+// Moving projects from the nav to across the top and
+// navigation overflow to the left side on desktop
 
 var isDesktop = window.outerWidth > 800,
 	projects = document.getElementById("projects")
 
 if(isDesktop) {
 	document.body.insertBefore(projects, document.body.firstChild)
+}
+
+// On Click CSS Class Toggles
+
+var clickToggles = document.querySelectorAll("[data-on-click-toggle-class]"),
+	clickTogglesLength = clickToggles.length
+
+for(var i = 0; i < clickTogglesLength; i++) {
+	var element = clickToggles[i]
+	clickToggles[i].addEventListener("click", function(event) {
+		var toggleClass = element.dataset.onClickToggleClass
+		if(element.classList.contains(toggleClass)) {
+			element.classList.remove(toggleClass)
+		} else {
+			element.classList.add(toggleClass)
+		}
+	})
 }
